f836e8bc81d00a0a40f07b8a5aad775441697275
jstricker98 <50211425+jstricker98@users.noreply.github.com>
2019-05-03 17:56:46 -0400

    Simplified game class

--- Game.h
+++ Game.h
@@ -2,38 +2,12 @@
 class Game
 {
 public:
+	Game(int, int);
+	int generateNumber(int);
 	bool guessIsCorrect(int);
 	bool outOfTurns();
 	void takeTurn();
 	int maxNumber;
 	int numberOfTurnsLeft;
 	int correctAnswer; 
-};
-
-class easyGame : public Game
-{
-public:
-	easyGame();
-	int generateNumberForEasy();
-};
-
-class mediumGame : public Game
-{
-public:
-	mediumGame();
-	int generateNumberForMedium();
-};
-
-class hardGame : public Game
-{
-public:
-	hardGame();
-	int generateNumberForHard();
-};
-
-class insaneGame : public Game
-{
-public:
-	insaneGame();
-	int generateNumberForInsane();
 };
